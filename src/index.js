8d0e66ac4b119190916061d25817422e933b708f
Chris <ju66ernautdev@gmail.com>
2018-11-12 07:10:54 -0800

    adding some styles, readability to completed alphabet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ let alphabetStr = "abcdefghijklmnopqrstuvwxyz";
 let alphabetArr = alphabetStr.split("");
 let completedAlphabet = [];
 
-document.getElementById("app").innerHTML = `
-  alphabet string:${alphabetStr} <br/>
-  alphabet array: ${alphabetArr} <br/>
-  completed alphabet: ${completedAlphabet}
-`;
+// document.getElementById("app").innerHTML = `
+//   alphabet string:${alphabetStr} <br/>
+//   alphabet array: ${alphabetArr} <br/>
+//   completed alphabet: ${completedAlphabet}
+// `;
 
 const handleKeydown = event => {
   //alert(String.fromCharCode(event.keyCode));
@@ -15,7 +15,10 @@ const handleKeydown = event => {
   if (keyLetter === nextLetter) {
     //    alert(keyLetter)
     completedAlphabet.push(keyLetter);
-    document.getElementById("completedAlphabet").innerHTML = completedAlphabet;
+    document.getElementById("completedAlphabet").innerHTML = completedAlphabet
+      .toString()
+      .split(",")
+      .join(" ");
     setTimeout(() => {
       if (completedAlphabet.length === 26) {
         alert("you did it");
